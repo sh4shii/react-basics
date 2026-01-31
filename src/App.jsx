a63e33b4831c17692eeef312895b8e97ce15efe1
Shashi Kumar <shashisaurav50@gmail.com>
2026-02-01 00:21:43 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
